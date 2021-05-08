3d247f162a04d14e9cfeb2604d37c26a4d6de9df
bladymir <bladymircch@gmail.com>
2021-05-08 13:21:58 -0500

    fix

--- src/main/java/com/hacom/demo/configuration/ConfigurationApp.java
+++ src/main/java/com/hacom/demo/configuration/ConfigurationApp.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "prueba")
+@ConfigurationProperties
 public class ConfigurationApp {
 
     private String mongodbDatabase;
